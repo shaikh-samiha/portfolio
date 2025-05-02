8867a32dae2055cb2fa2c78a1a4fa30e76985b45
shaikh-samiha <shaikhsamiha9175@gmail.com>
2025-05-03 00:24:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const toggleButton = document.getElementById('toggle-dark-mode');
-
-toggleButton.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
-    if (document.body
+const toggleBtn = document.getElementById('toggle-dark-mode');
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
